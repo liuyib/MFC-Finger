ee077a6b59633a625b4579770bd64ff70137102e
seriouszyx <674965440@qq.com>
2019-12-09 20:45:51 +0800

    feat: 使用skin++进行美化

--- Finger-app/Finger.cpp
+++ Finger-app/Finger.cpp
@@ -41,6 +41,7 @@ BOOL CFingerApp::InitInstance()
 
 
 	AfxEnableControlContainer();
+	skinppLoadSkin("OSXP.ssk");
 
 	// 创建 shell 管理器，以防对话框包含
 	// 任何 shell 树视图控件或 shell 列表视图控件。
--- Finger-app/stdafx.h
+++ Finger-app/stdafx.h
@@ -30,6 +30,8 @@
 #ifndef _AFX_NO_AFXCMN_SUPPORT
 #include <afxcmn.h>             // MFC 对 Windows 公共控件的支持
 #endif // _AFX_NO_AFXCMN_SUPPORT
+#include "SkinPPWTL.h"   //在这个地方加
+#pragma comment(lib,"SkinPPWTL.lib")
 
 #include <afxcontrolbars.h>     // 功能区和控件条的 MFC 支持
 
