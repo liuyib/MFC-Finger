1e2b86382e9c7fc7285fc6a4be1d93d032210a07
liuyib <1656081615@qq.com>
2019-12-10 15:35:59 +0800

    refactor: 更换皮肤&修改界面布局

--- Finger-app/Finger.cpp
+++ Finger-app/Finger.cpp
@@ -41,7 +41,7 @@ BOOL CFingerApp::InitInstance()
 
 
 	AfxEnableControlContainer();
-	skinppLoadSkin("./skin/UMskin.ssk");
+	skinppLoadSkin("./skin/RisingDragon.ssk");
 
 	// 创建 shell 管理器，以防对话框包含
 	// 任何 shell 树视图控件或 shell 列表视图控件。
